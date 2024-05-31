f9fc8b1022c57e7c107dcb64ac15986bd2238b16
chhavi6V <studychhavi03@gmail.com>
2024-05-31 12:00:25 +0530

    Admin panel updated

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -1,32 +1,124 @@
 const { listCollections, getCollectionSchema } = require('../utils/common/collections'); // Import collection utility functions
 
-async function getCollections(req, res){
+async function getReferralCodes(req, res){
   try {
-    const collections = await listCollections();
-    res.json(collections);
+    const referralCodes = await prisma.referralCode.findMany();
+    res.json(referralCodes);
   } catch (error) {
-    console.error('Error fetching collections:', error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ error: error.message });
   }
 };
 
-async function getData(req, res){
-    const collectionName = req.params.collectionName;
-    try {
-      const data = await getCollectionSchema(collectionName);
-      if (!data) {
-        return res.status(404).json({ message: `Collection '${collectionName}' not found or has no data` });
-      }
-      res.json(data);
-    } catch (error) {
-      console.error('Error fetching schema:', error);
-      res.status(500).json({ message: 'Server error' });
-    }
+async function getPlans(req, res){
+  try {
+    const plans = await prisma.plan.findMany();
+    res.json(plans);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
+async function getUsers(req, res){
+  try {
+    const users = await prisma.user.findMany({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+async function getSubscribedUsers(req, res){
+  try {
+    const nonFreeOrStudentUsers = await prisma.user.findMany({
+      where: {
+        planName: {
+          notIn: ["free", "student"],
+        },
+      },
+    });
+    res.json(nonFreeOrStudentUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+async function getModels(req, res){
+  try {
+    const models = await prisma.model.findMany();
+    res.json(models);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+async function getSessions(req, res){
+  try {
+    const sessions = await prisma.session.findMany();
+    res.json(sessions);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+async function getTopUsers(req, res){
+  try {
+    const topUsers = await prisma.user.findMany({
+      select: {
+        mongoId: true,
+        phoneNumber: true,
+        planName: true,
+        sessions: {
+          select: {
+            _count: true,
+          },
+        },
+      },
+      orderBy: {
+        sessions: {
+          _count: 'desc',
+        },
+      },
+      take: 10, // Limit the result to 7 users
+    });
+
+    const formattedUsers = topUsers.map(user => ({
+      ...user,
+      sessionCount: user.sessions._count,
+    }));
+
+    res.json(formattedUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+async function getMessages(req, res){
+  try {
+    const messages = await prisma.message.findMany();
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+async function getFeedbacks(req, res){
+  try {
+    const feedback = await prisma.feedback.findMany();
+    res.json(feedback);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
   
 module.exports = {
-  getCollections,
-  getData
+  getReferralCodes,
+  getPlans,
+  getUsers,
+  getSubscribedUsers,
+  getModels,
+  getSessions,
+  getMessages,
+  getFeedbacks,
+  getTopUsers
 }
