9e3279cd2a6bae3cac53f8deede97e88c7fba74b
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-08-06 14:58:32 +0530

    some bug fixed

--- src/services/courtroom-service.js
+++ src/services/courtroom-service.js
@@ -203,7 +203,7 @@ async function courtRoomBook(
       email: email,
     });
 
-    if (!trailBooking || trailBooking?.totalSlots > trailBooking?.bookedSlots) {
+    if (!trailBooking || trailBooking?.totalSlots < trailBooking?.bookedSlots) {
       console.log(
         `User with phone number ${phoneNumber} or email ${email} cannot book a slot at ${hour}:00 on ${bookingDate.toDateString()}.`
       );
