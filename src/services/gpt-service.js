a671dedc242883eef29b16e6e52a3459a4301345
Shubham Prajapati <shubham528prajapati@gmail.com>
2024-12-02 08:14:54 +0530

    bug resolved

--- src/services/gpt-service.js
+++ src/services/gpt-service.js
@@ -589,7 +589,7 @@ async function fetchGptUser(mongoId) {
       const expiresAt = new Date(createAt.getTime() + 30 * 24 * 60 * 60 * 1000);
 
       await updateUserPlan(
-        updatedClient.id,
+        mongoId,
         "FREE_M",
         "EVENT_OCCATION_FREE",
         "",
