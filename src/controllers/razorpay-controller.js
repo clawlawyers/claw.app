f5973483c5801752ae3415a697a35da5235ad226
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-01-21 18:23:33 +0530

    new thing

--- src/controllers/razorpay-controller.js
+++ src/controllers/razorpay-controller.js
@@ -655,8 +655,8 @@ async function createSubscription(req, res) {
 
   try {
     const subscriptionOptions = {
-      // plan_id: "plan_PkukrQkTdM9reT", // live
-      plan_id: "plan_PiqDuUsceqF696", // test
+      plan_id: "plan_PkukrQkTdM9reT", // live
+      // plan_id: "plan_PiqDuUsceqF696", // test
       customer_notify: 1,
       total_count: 12,
       notes: {
